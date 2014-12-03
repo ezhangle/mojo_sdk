69afae9952c90f37d776880f9fcfbd147def4eab
Jim Beveridge <jimbe@chromium.org>
2014-12-03 08:45:11 -0800

    Added MojoNewWait and MojoNewWaitMany, along with unit tests.
    R=viettrungluu@chromium.org
    BUG=365436
    
    Review URL: https://codereview.chromium.org/774673003
    
    Cr-Mirrored-From: https://github.com/domokit/mojo
    Cr-Mirrored-Commit: 4bd89c6adeed4ae33dfe57bdab934d609b15e979

--- c/system/tests/core_unittest_pure_c.c
+++ c/system/tests/core_unittest_pure_c.c
@@ -61,8 +61,15 @@ const char* MinimalCTest(void) {
   EXPECT_EQ(MOJO_RESULT_OK, MojoCreateMessagePipe(NULL, &handle0, &handle1));
 
   signals = MOJO_HANDLE_SIGNAL_READABLE;
+  uint32_t result_index = 1;
+  struct MojoHandleSignalsState states[1];
   EXPECT_EQ(MOJO_RESULT_DEADLINE_EXCEEDED,
-            MojoWaitMany(&handle0, &signals, 1, 1));
+            MojoNewWaitMany(&handle0, &signals, 1, 1, &result_index, states));
+
+  EXPECT_EQ(0u, result_index);
+  EXPECT_EQ(MOJO_HANDLE_SIGNAL_WRITABLE, states[0].satisfied_signals);
+  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE,
+            states[0].satisfiable_signals);
 
   EXPECT_EQ(MOJO_RESULT_OK,
             MojoWriteMessage(handle0,
@@ -72,9 +79,16 @@ const char* MinimalCTest(void) {
                              0u,
                              MOJO_WRITE_DATA_FLAG_NONE));
 
+  struct MojoHandleSignalsState state;
   EXPECT_EQ(
       MOJO_RESULT_OK,
-      MojoWait(handle1, MOJO_HANDLE_SIGNAL_READABLE, MOJO_DEADLINE_INDEFINITE));
+      MojoNewWait(handle1, MOJO_HANDLE_SIGNAL_READABLE,
+                  MOJO_DEADLINE_INDEFINITE, &state));
+
+  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE,
+            state.satisfied_signals);
+  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE,
+            state.satisfiable_signals);
 
   num_bytes = (uint32_t)sizeof(buffer);
   EXPECT_EQ(MOJO_RESULT_OK,
