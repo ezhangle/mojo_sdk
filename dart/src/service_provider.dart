8a2c8ff4db19c5691ccec8f5b84414bbacfa22bf
Scott Violet <sky@chromium.org>
2015-02-10 14:35:55 -0800

    Makes ServiceProvider (in dart) more extensible
    Now you can set a factory for a specific interface as well as a
    fallback. The fallback is passed the interface name.
    
    R=zra@google.com, zra@chromium.org
    
    Review URL: https://codereview.chromium.org/912053002
    
    Cr-Mirrored-From: https://github.com/domokit/mojo
    Cr-Mirrored-Commit: 3b6d20fcec7592e7db16beb8b611ee3c17cb9b79

--- dart/src/service_provider.dart
+++ dart/src/service_provider.dart
@@ -5,22 +5,44 @@
 part of application;
 
 typedef core.Listener ListenerFactory(core.MojoMessagePipeEndpoint endpoint);
+typedef core.Listener FallbackListenerFactory(
+        String interfaceName, core.MojoMessagePipeEndpoint endpoint);
 
 class ServiceProvider extends service_provider.ServiceProvider {
-  ListenerFactory factory;
+  FallbackListenerFactory fallbackFactory;
 
   service_provider.ServiceProviderProxy _proxy;
+
+  Map<String, ListenerFactory> _interfaceFactories;
 
   ServiceProvider(
       service_provider.ServiceProviderStub services,
       [service_provider.ServiceProviderProxy exposedServices = null])
       : _proxy = exposedServices,
+        _interfaceFactories = new Map(),
         super.fromStub(services) {
     delegate = this;
   }
 
-  connectToService(String interfaceName, core.MojoMessagePipeEndpoint pipe) =>
-      factory(pipe).listen();
+  connectToService(String interfaceName, core.MojoMessagePipeEndpoint pipe) {
+    if (_interfaceFactories.containsKey(interfaceName)) {
+      var listener = _interfaceFactories[interfaceName](pipe);
+      if (listener != null) {
+        listener.listen();
+        return;
+      }
+    }
+    if (fallbackFactory != null) {
+      var listener = fallbackFactory(interfaceName, pipe);
+      if (listener != null) {
+        listener.listen();
+        return;
+      }
+    }
+    // If we get here the interface isn't known. This is legal. Close the pipe
+    // so the remote side sees we don't support this interface.
+    pipe.handle.close();
+  }
 
   requestService(String name, bindings.Proxy clientImpl) {
     assert(_proxy != null);
@@ -28,6 +50,10 @@ class ServiceProvider extends service_provider.ServiceProvider {
     var pipe = new core.MojoMessagePipe();
     clientImpl.bind(pipe.endpoints[0]);
     _proxy.connectToService(name, pipe.endpoints[1]);
+  }
+
+  registerFactory(String interfaceName, ListenerFactory factory) {
+    _interfaceFactories[interfaceName] = factory;
   }
 
   close({bool nodefer : false}) {
