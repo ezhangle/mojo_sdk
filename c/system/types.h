aae5033a00ac409588b9ed2e23be104f70a36a44
Viet-Trung Luu <viettrungluu@chromium.org>
2015-04-06 14:17:41 -0700

    Revert "Revert "Make MojoResults positive.""
    This reverts commit d27bf39bbcc3606d5268581b88c0e6ede69cba47.
    
    And relands commit 3e048088e59228fa3f5ddcd69ce2b4119c8cdaa5
    (https://codereview.chromium.org/1059543002).
    
    This time: "Roll the network service to a custom build created from
    https://crrev.com/1060993004/#ps1".
    
    TBRing, since this is essentially the same change, except that qsr made
    some Android fixes, and this time I actually ran the tests locally (on
    Android).
    
    TBR=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1061033003
    
    Cr-Mirrored-From: https://github.com/domokit/mojo
    Cr-Mirrored-Commit: 787c01707d7cf24b121393e273d16bfc20287a4b

--- c/system/types.h
+++ c/system/types.h
@@ -93,42 +93,42 @@ typedef int32_t MojoResult;
 
 #ifdef __cplusplus
 const MojoResult MOJO_RESULT_OK = 0;
-const MojoResult MOJO_RESULT_CANCELLED = -1;
-const MojoResult MOJO_RESULT_UNKNOWN = -2;
-const MojoResult MOJO_RESULT_INVALID_ARGUMENT = -3;
-const MojoResult MOJO_RESULT_DEADLINE_EXCEEDED = -4;
-const MojoResult MOJO_RESULT_NOT_FOUND = -5;
-const MojoResult MOJO_RESULT_ALREADY_EXISTS = -6;
-const MojoResult MOJO_RESULT_PERMISSION_DENIED = -7;
-const MojoResult MOJO_RESULT_RESOURCE_EXHAUSTED = -8;
-const MojoResult MOJO_RESULT_FAILED_PRECONDITION = -9;
-const MojoResult MOJO_RESULT_ABORTED = -10;
-const MojoResult MOJO_RESULT_OUT_OF_RANGE = -11;
-const MojoResult MOJO_RESULT_UNIMPLEMENTED = -12;
-const MojoResult MOJO_RESULT_INTERNAL = -13;
-const MojoResult MOJO_RESULT_UNAVAILABLE = -14;
-const MojoResult MOJO_RESULT_DATA_LOSS = -15;
-const MojoResult MOJO_RESULT_BUSY = -16;
-const MojoResult MOJO_RESULT_SHOULD_WAIT = -17;
+const MojoResult MOJO_RESULT_CANCELLED = 1;
+const MojoResult MOJO_RESULT_UNKNOWN = 2;
+const MojoResult MOJO_RESULT_INVALID_ARGUMENT = 3;
+const MojoResult MOJO_RESULT_DEADLINE_EXCEEDED = 4;
+const MojoResult MOJO_RESULT_NOT_FOUND = 5;
+const MojoResult MOJO_RESULT_ALREADY_EXISTS = 6;
+const MojoResult MOJO_RESULT_PERMISSION_DENIED = 7;
+const MojoResult MOJO_RESULT_RESOURCE_EXHAUSTED = 8;
+const MojoResult MOJO_RESULT_FAILED_PRECONDITION = 9;
+const MojoResult MOJO_RESULT_ABORTED = 10;
+const MojoResult MOJO_RESULT_OUT_OF_RANGE = 11;
+const MojoResult MOJO_RESULT_UNIMPLEMENTED = 12;
+const MojoResult MOJO_RESULT_INTERNAL = 13;
+const MojoResult MOJO_RESULT_UNAVAILABLE = 14;
+const MojoResult MOJO_RESULT_DATA_LOSS = 15;
+const MojoResult MOJO_RESULT_BUSY = 16;
+const MojoResult MOJO_RESULT_SHOULD_WAIT = 17;
 #else
 #define MOJO_RESULT_OK ((MojoResult)0)
-#define MOJO_RESULT_CANCELLED ((MojoResult) - 1)
-#define MOJO_RESULT_UNKNOWN ((MojoResult) - 2)
-#define MOJO_RESULT_INVALID_ARGUMENT ((MojoResult) - 3)
-#define MOJO_RESULT_DEADLINE_EXCEEDED ((MojoResult) - 4)
-#define MOJO_RESULT_NOT_FOUND ((MojoResult) - 5)
-#define MOJO_RESULT_ALREADY_EXISTS ((MojoResult) - 6)
-#define MOJO_RESULT_PERMISSION_DENIED ((MojoResult) - 7)
-#define MOJO_RESULT_RESOURCE_EXHAUSTED ((MojoResult) - 8)
-#define MOJO_RESULT_FAILED_PRECONDITION ((MojoResult) - 9)
-#define MOJO_RESULT_ABORTED ((MojoResult) - 10)
-#define MOJO_RESULT_OUT_OF_RANGE ((MojoResult) - 11)
-#define MOJO_RESULT_UNIMPLEMENTED ((MojoResult) - 12)
-#define MOJO_RESULT_INTERNAL ((MojoResult) - 13)
-#define MOJO_RESULT_UNAVAILABLE ((MojoResult) - 14)
-#define MOJO_RESULT_DATA_LOSS ((MojoResult) - 15)
-#define MOJO_RESULT_BUSY ((MojoResult) - 16)
-#define MOJO_RESULT_SHOULD_WAIT ((MojoResult) - 17)
+#define MOJO_RESULT_CANCELLED ((MojoResult)1)
+#define MOJO_RESULT_UNKNOWN ((MojoResult)2)
+#define MOJO_RESULT_INVALID_ARGUMENT ((MojoResult)3)
+#define MOJO_RESULT_DEADLINE_EXCEEDED ((MojoResult)4)
+#define MOJO_RESULT_NOT_FOUND ((MojoResult)5)
+#define MOJO_RESULT_ALREADY_EXISTS ((MojoResult)6)
+#define MOJO_RESULT_PERMISSION_DENIED ((MojoResult)7)
+#define MOJO_RESULT_RESOURCE_EXHAUSTED ((MojoResult)8)
+#define MOJO_RESULT_FAILED_PRECONDITION ((MojoResult)9)
+#define MOJO_RESULT_ABORTED ((MojoResult)10)
+#define MOJO_RESULT_OUT_OF_RANGE ((MojoResult)11)
+#define MOJO_RESULT_UNIMPLEMENTED ((MojoResult)12)
+#define MOJO_RESULT_INTERNAL ((MojoResult)13)
+#define MOJO_RESULT_UNAVAILABLE ((MojoResult)14)
+#define MOJO_RESULT_DATA_LOSS ((MojoResult)15)
+#define MOJO_RESULT_BUSY ((MojoResult)16)
+#define MOJO_RESULT_SHOULD_WAIT ((MojoResult)17)
 #endif
 
 // |MojoDeadline|: Used to specify deadlines (timeouts), in microseconds (except
